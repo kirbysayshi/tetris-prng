84e52f96c8c321037ef65d6e11987c9d5d29cf67
Andrew Petersen <kirbysayshi@gmail.com>
2014-05-31 11:46:49 -0400

    added api, generation function, tests

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-function next(tap1, tap2, leftmostBitIndex, value) {
-  var bit1 = (value >> tap1) & 1;
-  var bit9 = (value >> tap2) & 1;
-  var leftmostBit = bit1 ^ bit9;
-  // >>> 0 is to ensure we don't become signed accidentally.
-  return ((leftmostBit << leftmostBitIndex) | (value >>> 1)) >>> 0;
+function lfsr(seed, taps, leftmostBitIndex) {
+  var tapDefs = taps.map(function(tap, i) {
+    return 'var bit' + i + ' = (value >> ' + tap + ') & 1;'
+  }).join('\n')
+
+  var tapXOR = taps.map(function(tap, i) {
+    return 'bit' + i;
+  }).join(' ^ ')
+
+  var body = ''
+    + 'var value = ' + seed + ' >>> 0;\n'
+    + 'return function() { \n'
+      + tapDefs + ' \n'
+      + 'var leftmostBit = ' + tapXOR + '; \n'
+      + 'return (value = ((leftmostBit << ' + leftmostBitIndex + ') | (value >>> 1)) >>> 0); \n'
+    + '} \n'
+
+  var fn = new Function(body);
+  return attachAPI(leftmostBitIndex, fn());
 }
 
-function seeded(seed, opt_tap1, opt_tap2, opt_leftmostBitIndex) {
-  opt_tap1 = opt_tap1 || 1;
-  opt_tap2 = opt_tap2 || 9;
-  opt_leftmostBitIndex = opt_leftmostBitIndex || 15;
-  var last = seed >>> 0; // ensure significant 32 bits are used
+function attachAPI(leftmostBitIndex, fn) {
+  var max = parseInt(Array(leftmostBitIndex+2).join(1), 2);
+  fn.maxValue = max;
+  fn.random = function() {
+    return (fn() / max);
+  }
+  return fn;
+}
+
+// Not worth golfing!
+// http://jsperf.com/tetris-prng-inlined-vs-variables
+
+function tetris() {
+  var value = 0x8988;
   return function() {
-    return (last = next(opt_tap1, opt_tap2, opt_leftmostBitIndex, last));
+    var bit1 = (value >> 1) & 1;
+    var bit9 = (value >> 9) & 1;
+    var leftmostBit = bit1 ^ bit9;
+    return (value = ((leftmostBit << 15) | (value >>> 1)) >>> 0);
   }
 }
 
-module.exports = seeded(0x8988, 1, 9, 15)
-module.exports.seeded = seeded;
+module.exports = attachAPI(15, tetris());
+module.exports.lfsr = lfsr;
--- test.js
+++ test.js
@@ -9,9 +9,28 @@ test('default seed', function(t) {
 })
 
 test('custom seed', function(t) {
-  var rand = tprng.seeded(1000);
+  var rand = tprng.lfsr(1000, [1, 9], 15);
   t.equal(rand(), 33268);
   t.equal(rand(), 16634);
   t.equal(rand(), 41085);
+  t.end();
+})
+
+test('range info', function(t) {
+
+  t.ok(tprng.random());
+
+  var rand = tprng.lfsr(1000, [1, 9], 15);
+  t.equal(rand.maxValue, 65535);
+  t.equal(rand.random(), 0.5076371404592965);
+  t.equal(rand.random(), 0.25381857022964827);
+  t.equal(rand.random(), 0.6269169146257725);
+
+  var rand = tprng.lfsr(1000, [1, 9], 31);
+  t.equal(rand.maxValue, 4294967295);
+  t.equal(rand.random(), 0.5000001165317371);
+  t.equal(rand.random(), 0.2500000582658686);
+  t.equal(rand.random(), 0.6250000292493496);
+
   t.end();
 })
